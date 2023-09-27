02d0ca1541eeb7feb559c9ada2ef5a65a5b09999
Michelle <tibiamichelle@gmail.com>
2023-09-27 15:03:12 -0300

    desafio2

---
+++ atividades/desafio-2-organizando-ativos.js
@@ -0,0 +1,17 @@
+// Entrada da quantidade de ativos
+const quantidadeAtivos = parseInt(gets());
+
+//Entrada dos tipos de ativos
+const ativos = [ ];
+for (let i = 0; i < quantidadeAtivos; i++) {
+  const codigoAtivo = gets();
+  ativos.push(codigoAtivo);
+}
+
+//TODO: Ordenar os ativos em ordem alfabética.
+ativos.sort();
+
+//TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
+ativos.forEach(codigoAtivo => {
+    print(codigoAtivo);
+  });
