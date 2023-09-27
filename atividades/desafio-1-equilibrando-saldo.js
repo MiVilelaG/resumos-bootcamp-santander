5f63af09c76da9ab3af700281ddd514ff1849d41
Michelle <tibiamichelle@gmail.com>
2023-09-27 14:47:17 -0300

    modificacao-desafio1

--- atividades/desafio-1-equilibrando-saldo.js
+++ atividades/desafio-1-equilibrando-saldo.js
@@ -1,8 +1,8 @@
 // Entrada do código: 3 entrada, saldo da conta, valor depositado e valor de saque
 
-const saldoAtual = 1000;
-const valorDeposito = 500;
-const valorSaque = 200;
+const saldoAtual = 1000
+const valorDeposito = 500
+const valorSaque = 200
 
 // Desenvolver um código para mostrar o valor do saldo final após o cliente fazer depositos e saques
 
@@ -10,4 +10,4 @@ const saldoFinal = saldoAtual + valorDeposito - valorSaque;
 
 //Saída do código: imprimir o valor final que ficou na conta do cliente
 
-console.log("Saldo atualizado da conta: " + saldoFinal)
+console.log("Saldo atualizado da conta: " + saldoFinal);
