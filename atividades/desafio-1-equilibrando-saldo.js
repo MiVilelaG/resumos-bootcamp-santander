d624208a74e701da368d27b90f16d99601c11694
MiVilelaG <105995942+MiVilelaG@users.noreply.github.com>
2023-09-27 14:34:07 -0300

    codigo js adicionado tarefa 1

---
+++ atividades/desafio-1-equilibrando-saldo.js
@@ -0,0 +1,13 @@
+// Entrada do código: 3 entrada, saldo da conta, valor depositado e valor de saque
+
+const saldoAtual = 1000;
+const valorDeposito = 500;
+const valorSaque = 200;
+
+// Desenvolver um código para mostrar o valor do saldo final após o cliente fazer depositos e saques
+
+const saldoFinal = saldoAtual + valorDeposito - valorSaque;
+
+//Saída do código: imprimir o valor final que ficou na conta do cliente
+
+console.log("Saldo atualizado da conta: " + saldoFinal)
